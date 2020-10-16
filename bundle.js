41bccdcdb94bae1249704313d1753e2bcef25782
Nate Wessel <bike756@gmail.com>
2020-10-16 15:59:59 -0400

    pan to results and show centroids

--- bundle.js
+++ bundle.js
@@ -7479,6 +7479,15 @@ var Marker = Layer.extend({
 	}
 });
 
+
+// factory L.marker(latlng: LatLng, options? : Marker options)
+
+// @factory L.marker(latlng: LatLng, options? : Marker options)
+// Instantiates a Marker object given a geographical point and optionally an options object.
+function marker(latlng, options) {
+	return new Marker(latlng, options);
+}
+
 /*
  * @class Path
  * @aka L.Path
@@ -14531,38 +14540,67 @@ function json(input, init) {
 }
 
 const server = 'http://localhost/interactive-geocoder/server';
+const locationIQ = 'https://us1.locationiq.com/v1/search.php';
+
+const form = {};
+
+var map;
 
 window.onload = ()=>{
+	// identify form elements
+	form.uid = select('input[name="uid"]');
+	form.region = select('input[name="region"]');
+	form.country = select('input[name="country"]');
+	form.province = select('input[name="province"]');
+	form.city = select('input[name="city"]');
+	
 	select('form button').on('click',geocode);
-	const m = createMap('map').setView([43.7,-79.4], 10);
+	// make a map
+	map = createMap('map');
 	tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
-		.addTo(m);
-	// get a place
+		.addTo(map);
+	// get a place up in the form
 	json(`${server}/fresh-place.php`)
 	.then( response => {
-		console.log(response);
-		select('input[name="uid"]').attr('value',response.uid);
-		select('input[name="region"]').attr('value',response.region);
-		select('input[name="country"]').attr('value',response.country);
-		select('input[name="province"]').attr('value',response.province);
-		select('input[name="city"]').attr('value',response.city);
+		console.log('Place is:',response);
+		form.uid.attr('value',response.uid);
+		form.region.attr('value',response.region);
+		form.country.attr('value',response.country);
+		form.province.attr('value',response.province);
+		form.city.attr('value',response.city);
+		// and pre-geocode it
+		geocode();
 	} );
 };
 
 function geocode(){
-	let data = {
-		'uid': select('input[name="uid"]').attr('value'),
-		'region': select('input[name="region"]').attr('value'),
-		'country': select('input[name="country"]').attr('value'),
-		'province': select('input[name="province"]').attr('value'),
-		'city': select('input[name="city"]').attr('value')
-	};
-	json(
-		`${server}/update.php`,
-		{ 
-			method:'POST', 
-			headers: { "Content-type": "application/json; charset=UTF-8"},
-			body: JSON.stringify(data) 
-		}
-	);
+	let params = new URLSearchParams( {
+		'key': 'pk.3acf24fbf7ce2d8e0239a9e882b4919b',
+	 	'format': 'json',
+		'country': form.country.attr('value'),
+		'state': form.province.attr('value'),
+		'city': form.city.attr('value'),
+		'polygon_geojson': 1,
+		'matchquality': 1,
+		'namedetails': 1
+	} );
+	json(`${locationIQ}?${params.toString()}`)
+		.then( response => {
+			console.log(response);
+			// show each result on the map
+			response.map( r => {
+				let coords = [ r.lat, r.lon ];
+				let popupHTML = `
+					<b>${r.display_name}</b><br>
+					<i>Quality:</i> ${r.matchquality.matchcode}, 
+					${r.matchquality.matchlevel}, 
+					${r.matchquality.matchtype}`;
+				marker(coords)
+					.addTo(map)
+					.bindPopup(popupHTML);
+			} );
+			let allPoints = response.map( r => [r.lat,r.lon] );
+			map.fitBounds(allPoints,{maxZoom:14,padding:[30,30]});
+		} )
+		.catch( err => console.warn(err) );
 }
