e09633a30e1ad85a70ade4ff8f3730e96770e4ef
Nate Wessel <bike756@gmail.com>
2020-10-15 21:07:44 -0400

    fetch random place and pre-fill form

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 import { map, tileLayer } from 'leaflet/src/Leaflet'
+import { select } from 'd3-selection'
+import { json } from 'd3-fetch'
 
 window.onload = ()=>{
 	const m = map('map').setView([43.7,-79.4], 10)
 	tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
 		.addTo(m);
+	// 
+	json('http://localhost/interactive-geocoder/server/fresh-place.php')
+	.then( response => {
+		console.log(response)
+		select('input#region').attr('value',response.region)
+		select('input#country').attr('value',response.country)
+		select('input#province').attr('value',response.province)
+		select('input#city').attr('value',response.city)
+	} )
 }
