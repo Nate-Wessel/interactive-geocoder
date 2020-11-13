94cbeb7e1c72531b2967454e3cee6aab021b1f92
Nate Wessel <bike756@gmail.com>
2020-11-12 21:08:26 -0500

    placing full risk project schema here

--- server/schema.sql
+++ server/schema.sql
@@ -1,40 +1,61 @@
+CREATE TABLE risk_categories (
+	uid serial PRIMARY KEY,
+	title text,
+	description text
+);
+
+CREAT TABLE risk_components (
+	uid serial PRIMARY KEY,
+	category integer REFERENCES risk_categories (uid),
+	title text,
+	description text
+);
+
 CREATE TABLE jurisdiction_types (
 	uid serial PRIMARY KEY,
-	"label" text UNIQUE
+	label text UNIQUE, -- e.g. 'city', 'province', 'country'
+	description text
 );
 
 CREATE TABLE jurisdictions (
 	geo_id serial PRIMARY KEY,
 	name text,
-	jurisdiction_type int REFERENCES jurisdiction_types (uid),
-	parent int REFERENCES jurisdictions (uid),
-	osm_id bigint UNIQUE,
+	type int REFERENCES jurisdiction_types (uid),
+	parent int REFERENCES jurisdictions (geo_id),
+	osm_id bigint UNIQUE, -- basis of all geometry fields
 	point geometry(POINT,4326),
 	full_polygon geometry(MULTIPOLYGON,4326),
-	land_polygon geometry(MULTIPOLYGON,4326),
+	land_polygon geometry(MULTIPOLYGON,4326), -- full polygon but clipped to land
 	notes text,
-	UNIQUE (name, parent)
+	UNIQUE (name, parent, type)
 );
 
-CREATE VIEW jurisdictions_meta AS 
-WITH RECURSIVE parents(orig_geo_id,geo_id,name,parent,level) AS (
-	SELECT geo_id AS orig_geo_id, geo_id, name, parent, 0 AS level
-	FROM jurisdictions 
-	UNION ALL
-	SELECT p.orig_geo_id, j.geo_id, j.name, j.parent, p.level+1
-	FROM jurisdictions AS j, parents AS p
-	WHERE j.geo_id = p.parent
-), addr AS (
-	SELECT 
-		orig_geo_id AS geo_id,
-		string_agg(name, ', ' ORDER BY level ASC) AS addr
-	FROM parents
-	GROUP BY orig_geo_id
-)
-SELECT 
-	j.geo_id, name, addr, label AS is_a
-FROM jurisdictions AS j
-JOIN addr AS a
-	ON j.geo_id = a.geo_id
-JOIN jurisdiction_types AS jt
-	ON j.jurisdiction_type = jt.uid;
+CREATE TABLE events (
+	uid serial PRIMARY KEY,
+	title text, -- display title, not necessarily title of article
+	archive_link text UNIQUE, -- link to news story archived on https://web.archive.org/ (if paywall not a problem)
+	archive_pdf text UNIQUE, -- reference to a local filesystem (if paywall a problem)
+	justification text -- brief description of why this event is being tracked
+);
+
+CREATE TABLE event_links (
+	uid serial PRIMARY KEY,
+	from_event integer REFERENCES events (uid),
+	to_event integer REFERENCES events (uid),
+	description text -- optional
+);
+
+CREATE TABLE risk_event_assessments (
+	uid serial PRIMARY KEY,
+	event_uid integer REFERENCES events (uid),
+	jurisdiction_uid integer REFERENCES jurisdictions (geo_id)
+	component_uid integer REFERENCES risk_components (uid),
+	assessment smallint CHECK ( assessment >= -5 AND assessment <= 5 ) -- likert scales
+);
+
+CREATE TABLE risk_changes (
+	uid serial PRIMARY KEY,
+	description text,
+);
+
+
