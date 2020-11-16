02ef2742a00d83e6fa24b2a62f8988a0d1dcd33b
Nate Wessel <bike756@gmail.com>
2020-11-16 12:43:58 -0500

    add indexes

--- server/schema.sql
+++ server/schema.sql
@@ -12,7 +12,9 @@ CREATE TABLE jurisdictions (
 	osm_id bigint UNIQUE, -- basis of all geometry fields
 	point geometry(POINT,4326),
 	full_polygon geometry(MULTIPOLYGON,4326),
-	land_polygon geometry(MULTIPOLYGON,4326), -- full polygon but clipped to land
+	land_polygon geometry(MULTIPOLYGON,4326), -- clipped to coastline
 	notes text,
 	UNIQUE (name, parent, type)
 );
+CREATE INDEX ON jurisdictions USING GIST(full_polygon);
+CREATE INDEX ON jurisdictions USING GIST(land_polygon);
