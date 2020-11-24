caf61be17d082ebe170ff87d52314e6dbb5930ee
Nate Wessel <bike756@gmail.com>
2020-11-24 15:57:29 -0500

    configure react

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,37 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from 'rollup-plugin-json';
-import terser from "rollup-plugin-terser";
+//import terser from "rollup-plugin-terser";
+import babel from '@rollup/plugin-babel';
+import replace from '@rollup/plugin-replace';
 
 export default {
-	input: 'main.js',
+	input: 'src/main.js',
 	output: [ 
 		{
-			file: 'bundle.js',
-		},{
-			file: 'bundle.min.js',
-			plugins: [ terser.terser() ]
+			file: 'dist/bundle.js',
 		}
+		//,{
+		//	file: 'dist/bundle.min.js',
+		//	plugins: [ terser.terser() ]
+		//}
 	],
-	plugins: [ resolve(), commonjs(), json() ],
+	plugins: [ 
+		resolve(),
+		replace({
+			'process.env.NODE_ENV': JSON.stringify( 'development' )
+		}),
+		babel({ 
+			exclude: 'node_modules/**',
+			presets: ['@babel/env', '@babel/preset-react'],
+			babelHelpers: 'bundled'
+		}),
+		commonjs(),
+		json()
+	],
+	externals: {
+		'react': 'React'
+	},
 	onwarn: function (warning, warn) {
 		if (warning.code === 'CIRCULAR_DEPENDENCY') return;
 		warn(warning);
