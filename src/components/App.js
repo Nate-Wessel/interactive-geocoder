2eae49494ba29cec5f7758bd9207ee45b86d256d
Nate Wessel <bike756@gmail.com>
2020-11-26 09:42:09 -0500

    display place relations

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,15 @@ import React from 'react'
 import Search from './Search.js'
 import SelectedPlace from './SelectedPlace.js'
 
+import { json } from 'd3-fetch'
+
 export default class App extends React.Component {
 	constructor(props) { 
 		super(props)
 		this.state = {
-			selectedPlace: null
+			selectedPlace: null,
+			selectedPlaceChildren: [],
+			selectedPlaceSiblings: []
 		}
 		this.handlePlaceSelection = this.handlePlaceSelection.bind(this);
 		this.showSelectedPlace = this.showSelectedPlace.bind(this);
@@ -16,6 +20,8 @@ export default class App extends React.Component {
 			return (
 				<SelectedPlace 
 					place={this.state.selectedPlace}
+					children={this.state.selectedPlaceChildren}
+					siblings={this.state.selectedPlaceSiblings}
 					onNewPlaceSelection={this.handlePlaceSelection}/>
 			)
 		}else{
@@ -26,6 +32,20 @@ export default class App extends React.Component {
 	handlePlaceSelection(place){
 		console.log('new place selected',place)
 		this.setState({selectedPlace:place})
+		this.setState({selectedPlaceChildren:[]})
+		this.setState({selectedPlaceSiblings:[]})
+		// fetch data on relations
+		json(`./server/jurisdiction.php?parent=${place.geo_id}`)
+			.then( children => { 
+				this.setState({selectedPlaceChildren:children}) 
+			} )
+		if(place.parent){
+			json(`./server/jurisdiction.php?parent=${place.parent.geo_id}`)
+				.then( response => {
+					siblings = response.filter(sib=>sib.geo_id!=place.geo_id) 
+					this.setState({selectedPlaceSiblings:siblings}) 
+				} )
+		}
 	}
 	render() {	
 		return (
--- src/components/SelectedPlace.js
+++ src/components/SelectedPlace.js
@@ -1,50 +1,35 @@
 import React from 'react'
 import PlacesList from './PlacesList.js'
-import { json } from 'd3-fetch'
 
 export default class SelectedPlace extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = { 
-			children:[],
-			siblings:[]
-		}
-	}
-
 	render() {
-		let currentPlace = this.props.place
+		let place = this.props.place
+		// ancestors are stored directly with the place
+		let currentPlace = place
 		let ancestors = []
 		while(currentPlace.parent){ 
 			ancestors.unshift(currentPlace=currentPlace.parent) 
 		}
-		let place = this.props.place
 		return (
 			<div id="selected-place">
 				<h1>{place.name}</h1>
 				<p>
-					<b>Type:</b>{place.type_of}<br/>
-					<b>geo_id:</b>{place.geo_id}<br/>
-					<b>OSM_id:</b>{place.osm_id}
+					<b>Type:</b> {place.type_of}<br/>
+					<b>geo_id:</b> {place.geo_id}<br/>
+					<b>OSM_id:</b> {place.osm_id}
 				</p>
 				<div id="relations">
 					<PlacesList id="parents" 
 						onSelection={this.props.onNewPlaceSelection}
 						places={ancestors}/>
-					<PlacesList id="children"
-						onSelection={this.props.onNewPlaceSelection}
-						places={this.state.children}/>
 					<PlacesList id="siblings" 
 						onSelection={this.props.onNewPlaceSelection}
-						places={this.state.siblings}/>
+						places={this.props.siblings}/>
+					<PlacesList id="children"
+						onSelection={this.props.onNewPlaceSelection}
+						places={this.props.children}/>
 				</div>
 			</div>
 		)
 	}
 }
-/*
-		// async calls to get state from server
-		json(`./server/jurisdiction.php?parent=${props.place.geo_id}`)
-			.then(response=>{
-				this.setState({children:response})
-			})
-*/
