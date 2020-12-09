187b2e6c7e7e5f537f716f80ea0ef549c64741fd
Nate Wessel <bike756@gmail.com>
2020-12-09 11:52:38 -0500

    refactor to use hooks

--- src/components/RelatedPlacesList.jsx
+++ src/components/RelatedPlacesList.jsx
@@ -1,65 +1,64 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { displayName } from './placeDisplayName.js'
 import { json } from 'd3-fetch'
 
-export default class RelatedPlacesList extends Component{
-	constructor(props) { 
-		super(props)
-		this.state = {
-			collapsed: true,
-			havePlaces: props.places ? true : false,
-			places: props.places ? props.places : []
-		}
-		this.toggleDisplay = this.toggleDisplay.bind(this)
-		this.fetchRelations = this.fetchRelations.bind(this)
-	}
-	toggleDisplay(){
-		// check if we need to get data still
-		if( this.state.collapsed && ! this.state.havePlaces ){
-			this.fetchRelations()
-		}
-		this.setState({collapsed:!this.state.collapsed})
-	}
-	fetchRelations(){
-		this.setState({havePlaces: true})
-		if(this.props.parent){
-			console.log(`requesting children of ${this.props.parent.geo_id}`)
-			json(`./server/jurisdiction.php?parent=${this.props.parent.geo_id}`)
-				.then( children => { 
-					children.sort((a,b)=> a.name < b.name ? -1 : 1 )
-					this.setState( { places: children } )
-				} )
-		}else if(this.props.sibling){
-			let parent = this.props.sibling.parent
-			if(parent){
-				json(`./server/jurisdiction.php?parent=${parent.geo_id}`)
-					.then( children => { 
-						let siblings = children
-							.filter(sib=>sib.geo_id!=this.props.sibling.geo_id)
-							.sort((a,b)=> a.name < b.name ? -1 : 1 )
-						this.setState( { places: siblings } )
-					} )
-			}
-		}
-	}
-	render(){
-		let title = <h3 onClick={this.toggleDisplay}>{this.props.title}</h3>
-		if(this.state.collapsed){ 
-			return <div>{title}</div> 
-		}
-		let places = this.state.places.map( place => {
-			return (
-				<li key={place.geo_id}
-					onClick={(event)=>this.props.onSelection(place)}>
-					{displayName(place)}
-				</li>
-			)
-		})
+export default function RelatedPlacesList(props){
+	const [collapsed,setCollapsed] = useState(true)
+	const [places,setPlaces] = useState([])
+
+	useEffect(()=>{
+		// get the related places
+		if(props.child){ getAncestors(props.child) }
+		else if(props.sibling){ getSiblings(props.sibling) }
+		else if(props.parent){ getChildren(props.parent) }
+		// reset on update to props
+		return () => { setPlaces([]) }
+	},[props.child,props.sibling,props.parent])
+
+	let title = (
+		<h3 onClick={()=>setCollapsed(!collapsed)}>
+			{`${props.title} (${places.length})`}
+		</h3>
+	)
+	
+	if(collapsed){ return <div>{title}</div> }
+	
+	let placesList = places.map( place => {
 		return (
-			<div>
-				{title}
-				<ul className="place-list">{places}</ul>
-			</div>
+			<li key={place.geo_id}
+				onClick={(event)=>props.onSelection(place)}>
+				{displayName(place)}
+			</li>
 		)
+	})
+	return (
+		<div>
+			{title}
+			<ul className="place-list">{placesList}</ul>
+		</div>
+	)
+	function getAncestors(place){
+		// ancestors are stored directly with the place
+		let ancestors = []
+		while(place.parent){ ancestors.unshift(place=place.parent) }
+		setPlaces(ancestors)
+	}
+	function getSiblings(place){
+		if(place.parent){
+			json(`./server/jurisdiction.php?parent=${place.parent.geo_id}`)
+				.then( children => { 
+					let siblings = children
+						.filter(sib=>sib.geo_id!=place.geo_id)
+						.sort((a,b)=> a.name < b.name ? -1 : 1 )
+					setPlaces(siblings)
+				} )
+		}
+	}
+	function getChildren(place){
+		json(`./server/jurisdiction.php?parent=${place.geo_id}`)
+			.then( children => { 
+				children.sort((a,b)=> a.name < b.name ? -1 : 1 )
+				setPlaces(children)
+			} )	
 	}
 }
--- src/components/SelectedPlace.jsx
+++ src/components/SelectedPlace.jsx
@@ -2,30 +2,20 @@ import React, { Component }from 'react'
 import RelatedPlacesList from './RelatedPlacesList.jsx'
 
 export default function SelectedPlace(props) {
-	let place = props.place
-	// ancestors are stored directly with the place
-	let currentPlace = place
-	let ancestors = []
-	while(currentPlace.parent){ 
-		ancestors.unshift(currentPlace=currentPlace.parent) 
-	}
 	return (
 		<div id="selected-place">
-			<h1>{place.name}</h1>
+			<h1>{props.place.name}</h1>
 			<p>
-				<b>Type:</b> {place.type_of}<br/>
-				<b>geo_id:</b> {place.geo_id}<br/>
-				<b>OSM_id:</b> {place.osm_id}
+				<b>Type:</b> {props.place.type_of}<br/>
+				<b>geo_id:</b> {props.place.geo_id}<br/>
+				<b>OSM_id:</b> {props.place.osm_id}
 			</p>
 			<div id="relations">
-				<RelatedPlacesList title="Parents" 
-					onSelection={props.onNewPlaceSelection}
-					places={ancestors}/>
-				<RelatedPlacesList title="Siblings" 
-					sibling={props.place}
+				<RelatedPlacesList title="Parents"  child={props.place}
 					onSelection={props.onNewPlaceSelection}/>
-				<RelatedPlacesList title="Children"
-					parent={props.place} 
+				<RelatedPlacesList title="Siblings" sibling={props.place}
+					onSelection={props.onNewPlaceSelection}/>
+				<RelatedPlacesList title="Children" parent={props.place} 
 					onSelection={props.onNewPlaceSelection}/>
 			</div>
 		</div>
