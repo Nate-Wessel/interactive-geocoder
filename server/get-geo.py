9a4fb741cead84b4d91a4756413df85cdd416064
Nate Wessel <bike756@gmail.com>
2020-12-29 15:55:17 -0500

    more explicitly populate data from OSM

--- server/get-geo.py
+++ server/get-geo.py
@@ -2,6 +2,7 @@ from requests import get
 from osm2geojson import json2shapes
 from shapely.wkb import dumps as dumpWKB
 import psycopg2
+from psycopg2.extras import register_hstore
 from time import sleep
 
 conn_str = "host='localhost' dbname='APFC' user='nate' password='mink'"
@@ -9,19 +10,23 @@ connection = psycopg2.connect(conn_str)
 connection.autocommit = True
 cursor = connection.cursor()
 
+register_hstore(connection)
+
 overpass_url = "http://overpass-api.de/api/interpreter"
 
 
 cursor.execute("""
-	SELECT geo_id, osm_id 
+	SELECT geo_id, osm_id, name
 	FROM jurisdictions 
 	WHERE 
-		full_polygon IS NULL AND
-		osm_id IS NOT NULL;"""
+		osm_id IS NOT NULL AND 
+		osm_update IS NULL AND 
+		geo_id != 42
+	LIMIT 40;"""
 )
-for ( geo_id, osm_id ) in cursor.fetchall():
-	print('working on',geo_id,osm_id)
-
+for ( geo_id, osm_id, name ) in cursor.fetchall():
+	print('%s (%s) osm: %s'%(name,geo_id,osm_id))
+	# gather the data
 	overpass_query = """
 		[out:json][timeout:25];
 		rel(id:{});
@@ -30,25 +35,56 @@ for ( geo_id, osm_id ) in cursor.fetchall():
 		out skel qt;
 	""".format(osm_id)
 
-	response = get(overpass_url,params={'data': overpass_query})
-	data = json2shapes(response.json())
+	response = get(overpass_url,params={'data': overpass_query}).json()
 
-	for datum in data:
-		if datum['properties']['type'] == 'relation':
-			cursor.execute(
-				"""
-					UPDATE jurisdictions 
-					SET full_polygon = ST_SetSRID(
-						ST_GeomFromWKB(%(polygon_wkb)s),
-						4326
-					)
-					WHERE geo_id = %(geo_id)s;
-				""",
-				{
-					'geo_id':geo_id,
-					'polygon_wkb': dumpWKB(datum['shape'])
-				}
-			)
-			
+	if len(response['elements']) == 0:
+		print('no data returned by query for relation',osm_id)
+		break
+
+	# parse the data
+	for element in response['elements']:
+		if element['id'] == osm_id: # is the boundary relation
+			tags = element['tags']
+			labelID = centreID = -1
+			for member in element['members']:
+				if member['role'] == 'label': 
+					labelID = member['ref']
+				if member['role']	== 'admin_centre':
+					centreID = member['ref']
 			break
-	sleep(3)
+			
+	print('label: %s, admin_centre: %s'%(labelID,centreID))
+	
+	geojson = json2shapes(response)
+
+	label = admin_centre = None
+	for feature in geojson:
+		if feature['properties']['type'] == 'relation':
+			polygon = dumpWKB(feature['shape'])
+		elif feature['properties']['id'] == labelID:
+			label = dumpWKB(feature['shape'])
+		elif feature['properties']['id'] == centreID:
+			admin_centre = dumpWKB(feature['shape'])
+	
+	cursor.execute(
+		"""
+			UPDATE jurisdictions 
+			SET 
+				osm_update = NOW(),
+				osm_polygon = ST_SetSRID( ST_GeomFromWKB(%(polygon_wkb)s), 4326),
+				osm_tags = %(tags)s,
+				osm_label = ST_SetSRID( ST_GeomFromWKB(%(label)s), 4326),
+				osm_admin_centre = ST_SetSRID( ST_GeomFromWKB(%(admin_centre)s), 4326)
+			WHERE geo_id = %(geo_id)s;
+		""",
+		{
+			'geo_id':geo_id,
+			'polygon_wkb': polygon,
+			'tags': tags,
+			'label': label,
+			'admin_centre': admin_centre
+		}
+	)
+	
+			
+	sleep(1)
--- server/schema.sql
+++ server/schema.sql
@@ -10,11 +10,15 @@ CREATE TABLE jurisdictions (
 	type int NOT NULL REFERENCES jurisdiction_types (uid),
 	parent int REFERENCES jurisdictions (geo_id),
 	osm_id bigint UNIQUE, -- basis of all geometry fields
+	notes text,
+	osm_update timestamptz, -- time when osm-derived data was pulled
+	osm_tags hstore, -- OSM tags
 	point geometry(POINT,4326),
-	full_polygon geometry(MULTIPOLYGON,4326),
+	osm_polygon geometry(MULTIPOLYGON,4326),
+	osm_label geometry(POINT,4326),
+	osm_admin_centre geometry(POINT,4326),
 	land_polygon geometry(MULTIPOLYGON,4326), -- clipped to coastline
 	simplified_10m_polygon geometry(MULTIPOLYGON,4326),
-	notes text,
 	UNIQUE (name, parent, type)
 );
 CREATE INDEX ON jurisdictions USING GIST(full_polygon);
