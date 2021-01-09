0609483053936fc9e9fac6f969c882064cfa90e5
Nate Wessel <bike756@gmail.com>
2021-01-08 23:37:30 -0500

    simplify styling

--- src/components/SelectedPlace.jsx
+++ src/components/SelectedPlace.jsx
@@ -4,18 +4,22 @@ import AddChildForm from './AddChildForm.jsx'
 
 export default function SelectedPlace(props) {
 	return (
+		<div>
 		<div id="selected-place">
 			<h1>{props.place.name}</h1>
 			<div><b>Type:</b> {props.place.type_of}</div>
 			<div><b>geo_id:</b> {props.place.geo_id}</div>
 			<div><b>OSM_id:</b> {props.place.osm_id}</div>
-			<div>
-				<a href={`https://www.openstreetmap.org/relation/${props.place.osm_id}`}
-					target="_blank">
-					OSM relation
-				</a>
-			</div>
+			<h3>Outside links</h3>
 			<ul>
+				{props.place.osm_id &&
+					<li>
+						<a href={`https://www.openstreetmap.org/relation/${props.place.osm_id}`}
+							target="_blank">
+							OpenStreetMap
+						</a>
+					</li>
+				}
 				{props.place.wiki && 
 					<li>
 						<a href={`https://en.wikipedia.org/wiki/${props.place.wiki}`}
@@ -32,14 +36,15 @@ export default function SelectedPlace(props) {
 			<AddChildForm 
 				parent={props.place}
 				onAddition={props.onNewPlaceSelection}/>
-			<div id="relations">
+		</div>
+		<div id="relations">
 				<RelatedPlacesList title="Parents"  child={props.place}
 					onSelection={props.onNewPlaceSelection}/>
 				<RelatedPlacesList title="Siblings" sibling={props.place}
 					onSelection={props.onNewPlaceSelection}/>
 				<RelatedPlacesList title="Children" parent={props.place} 
 					onSelection={props.onNewPlaceSelection}/>
-			</div>
+		</div>
 		</div>
 	)
 }
