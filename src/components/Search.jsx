3372a97c832a8099fb7e1dba4aa545eea9dbff32
Nate Wessel <bike756@gmail.com>
2021-01-15 14:47:06 -0500

    refactor as function

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,47 +1,39 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { json } from 'd3-fetch' 
 import PlacesList from './PlacesList.jsx'
 
-export default class Search extends Component {
-	constructor(props) {
-		super(props)
-		this.state = { 
-			searchTerm: '',
-			searchResults: []
-		}
-		this.handleInput = this.handleInput.bind(this);
-	}
-	handleInput(event) {
-		this.setState({searchTerm:event.target.value})
+export default function(props) {
+	const [ searchTerm, setSearchTerm ] = useState('')
+	const [ searchResults, setSearchResults ] =  useState([])
+	
+	return (
+		<div className="search">
+			<input 
+				type="text"
+				placeholder="Search by place name or ID"   
+				value={searchTerm}
+				onInput={handleInput}/>
+			<PlacesList id="search-results"
+				places={searchResults}/>
+		</div>
+	)
+	
+	function handleInput(event) {
+		setSearchTerm(event.target.value)
 		
 		let text = event.target.value.trim()
 		if(isNaN(text) && text.length < 2){
-			return this.setState({searchResults:[]})
+			return setSearchResults([])
 		}
 		if(!isNaN(text) & Number(text) > 0){
 			json(`./server/jurisdiction.php?geo_id=${Number(text)}`)
-				.then(response => {
+				.then( response => {
 					// response can be 'undefined'
-					this.setState({ searchResults: response ? [response] : [] })
+					setSearchResults( response ? [response] : [] )
 				} )
 		}else{
 			json(`./server/jurisdiction.php?search=${text}`)
-				.then(response => {
-					this.setState({searchResults:response})
-				} )
+				.then( response => setSearchResults(response) )
 		}
-	}
-	render() {	
-		return (
-			<div className="search">
-				<input 
-					type="text"
-					placeholder="Search by place name or ID"   
-					value={this.state.searchTerm}
-					onInput={this.handleInput}/>
-				<PlacesList id="search-results"
-					places={this.state.searchResults}/>
-			</div>
-		)
 	}
 }
