79114bca75ca9308143dabd45b3d0e50af3a325a
Nate Wessel <bike756@gmail.com>
2021-01-16 00:54:02 -0500

    creat basic nav links

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,10 +1,17 @@
 import React, { useState } from 'react'
-import { Link, Switch } from 'react-router-dom'
-import { HashRouter as Router, Route } from 'react-router-dom'
+import { 
+	HashRouter as Router, 
+	Switch, 
+	Route, 
+	NavLink,
+	useParams
+} from 'react-router-dom'
+
 import Search from '../Search'
 import SelectedPlace from '../SelectedPlace'
 import Relations from '../SelectedPlace/Relations'
 import AddChildForm from '../AddChildForm.jsx'
+
 import './main.css'
 
 export default function JurisdictionManager(props) {
@@ -13,19 +20,33 @@ export default function JurisdictionManager(props) {
 			<Router>
 				<Switch>
 					<Route path="/:geo_id/addChild">
+						<Nav/>
 						<SelectedPlace/>
 						<AddChildForm/>
 					</Route>
 					<Route path="/:geo_id/relations">
+						<Nav/>
 						<SelectedPlace/>
 						<Relations/>
 					</Route>
 					<Route path="/:geo_id">
+						<Nav/>
 						<SelectedPlace/>
 					</Route>
 					<Route path="/" component={Search}/>
 				</Switch>
 			</Router>
 		</div>
+	)
+}
+
+function Nav(props){
+	const { geo_id } = useParams()
+	return (
+		<nav>
+			<NavLink to="/">Search</NavLink>
+			<NavLink to={`/${geo_id}/relations`}>Relations</NavLink>
+			<NavLink to={`/${geo_id}/addChild`}>Add Child</NavLink>
+		</nav>
 	)
 }
