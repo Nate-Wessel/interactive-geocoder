7c7ded425fba84c71a5d9dd14cb44336c4783883
Nate Wessel <bike756@gmail.com>
2021-01-17 20:36:31 -0500

    only show one set of relations at a time

--- src/SelectedPlace/Relations/index.jsx
+++ src/SelectedPlace/Relations/index.jsx
@@ -4,36 +4,39 @@ import { displayName } from '../../placeDisplayName.js'
 import PlacesList from '../../PlacesList' 
 import { json } from 'd3-fetch'
 import { publicAPI } from '../../API.js'
+import './relations.css'
 
 export default function(){
 	const { geo_id } = useParams()
+	const [ expanded, setExpanded ] = useState('Parents')
 	return (
 		<div id="relations">
-			<RelatedPlaces title="Parents"  child={geo_id}/>
-			<RelatedPlaces title="Siblings" sibling={geo_id}/>
-			<RelatedPlaces title="Children" parent={geo_id}/>
+			<RelatedPlaces title="Parents" child={geo_id} 
+				expand={setExpanded} expanded={expanded=='Parents'}/>
+			<RelatedPlaces title="Siblings" sibling={geo_id} 
+				expand={setExpanded} expanded={expanded=='Siblings'}/>
+			<RelatedPlaces title="Children" parent={geo_id} 
+				expand={setExpanded} expanded={expanded=='Children'}/>
 		</div>
 	)
 }
 
 function RelatedPlaces(props){
-	const [collapsed,setCollapsed] = useState(true)
 	const [places,setPlaces] = useState([])
-
 	useEffect(()=>{
 		// get the related places
 		if(props.child){ getAncestors(props.child) }
 		else if(props.sibling){ getSiblings(props.sibling) }
 		else if(props.parent){ getChildren(props.parent) }
-		else{ console.log('no props') }
+		else{ console.warn('no props') }
 	},[props])
 	
 	return (
 		<div>
-			<h3 onClick={()=>setCollapsed(!collapsed)}>
+			<h3 onClick={ () => props.expand(props.title) }>
 				{`${props.title} (${places.length})`}
 			</h3>
-			{ ! collapsed && <PlacesList places={places}/> }
+			{ props.expanded && <PlacesList places={places}/> }
 		</div>
 	)
 	
